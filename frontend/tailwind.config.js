2e40d2971de0973a476407b793a45200697c29c5
hagoromo2000 <revtm000126@gmail.com>
2023-02-14 21:59:11 +0900

    Add:Tailwindの環境構築 #3

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
